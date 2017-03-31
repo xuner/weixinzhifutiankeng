a7f8711e54522966b8f52de67c9ab2bc96314e75
xuner <1491913453@qq.com>
2017-03-31 13:55:24 +0800

    Update WxPay.pub.config.php

--- WxPay.pub.config.php
+++ WxPay.pub.config.php
@@ -2,13 +2,13 @@
 class WxPayConf_pub {
 	//=======【基本信息设置】=====================================
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-	const APPID = "wx123456845855";
+	const APPID = "wx123456845585550";
 	//受理商ID，身份标识
-	const MCHID = '11551258522';
+	const MCHID = '1155125852540';
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	const KEY = 'aidnfasfiaslfaisfnlsda5665';
+	const KEY = 'aidnfasfiaslfai5485sfnlsda5665';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-	const APPSECRET = "3515asdfe13ds55485dasf5";  
+	const APPSECRET = "3515asdfe1336548ds55485dasf5";  
 
 	//=======【JSAPI路径设置】===================================
 	//获取access_token过程中的跳转uri，通过跳转将code传入jsapi支付页面
